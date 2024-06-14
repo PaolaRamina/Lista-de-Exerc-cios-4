6e3fce14bb83a96b9b6bebd30509a16a2d9a08c0
Paola Ramina <paolaramina153@gmail.com>
2024-06-14 20:38:19 -0300

    Update Exercicio_IPEADATA.py

--- pages/Exercicio_IPEADATA.py
+++ pages/Exercicio_IPEADATA.py
@@ -19,5 +19,5 @@ selic
 fig, ax = plt.subplots()
 ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))")
 ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))")
-df.plot(ax=ax)
+ip.plot(ax=ax)
 st.pyplot(fig)
