9c4305add7936d8f092f7c08076f2b1bafec1d35
Paola Ramina <paolaramina153@gmail.com>
2024-06-14 19:43:20 -0300

    Add files via upload

---
+++ pages/Exercicio IPEADATA.py
@@ -0,0 +1,20 @@
+import ipeadatapy as ip
+
+# Exercício IPEADATA
+st.set_page_config(
+page_title="Exercício IPEADATA",
+page_icon="📆", 
+)
+
+ip.list_series('Selic')
+
+#####
+
+selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
+selic
+
+####
+
+ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))")
+ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))")
+plt.show()
