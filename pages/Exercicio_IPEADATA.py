910e6ae628b376c54a6bcea304675dec69bb2dbb
Paola Ramina <paolaramina153@gmail.com>
2024-06-14 21:04:43 -0300

    Update Exercicio_IPEADATA.py

--- pages/Exercicio_IPEADATA.py
+++ pages/Exercicio_IPEADATA.py
@@ -11,11 +11,12 @@ page_icon="📆",
 ip.list_series('Selic')
 
 #####
-st.write("Resultados para o indicador Taxa de juros - Over / Selic - acumulada no mês na base da IPEADATA")
+st.subheader("Resultados para o indicador Taxa de juros - Over / Selic - acumulada no mês na base da IPEADATA")
 selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
 selic
 
 ####
+st.subheader("Nos anos de 2022 e 2023:")
 fig, ax = plt.subplots()
 ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))")
 ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))",ax=ax)
